32a200dc772cb134cfbc9530fb964e1af073f526
Nik Krumm <nkrumm@gmail.com>
2013-10-30 16:06:44 -0700

    Don't calculate number of slots in cluster
    SGEPlugin can safely assume there are 9999 (or higher!) slots in the SGE
    cluster-- this has no effect on scheduling.

--- starcluster/plugins/sge.py
+++ starcluster/plugins/sge.py
@@ -61,8 +61,9 @@ class SGEPlugin(clustersetup.DefaultClusterSetup):
         log.info("%s SGE parallel environment '%s'" % (verb, name))
         # iterate through each machine and count the number of processors
         nodes = nodes or self._nodes
-        num_processors = sum(self.pool.map(lambda n: n.num_processors, nodes,
-                                           jobid_fn=lambda n: n.alias))
+        #num_processors = sum(self.pool.map(lambda n: n.num_processors, nodes,
+        #                                   jobid_fn=lambda n: n.alias))
+        num_processors = 9999
         if not pe_exists:
             penv = mssh.remote_file("/tmp/pe.txt", "w")
             penv.write(sge.sge_pe_template % (name, num_processors))
